80dce98e510c59dfeefd79c46a2b604da0fd5d72
Ruggery Gusmão <ruggery402@gmail.com>
2024-02-17 10:28:39 -0300

    Atualizar o App.jsx
    Atualizando jogadores

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,19 @@ function App() {
       <h1 className="title">PELADA DOS AMIGOS - 03/02/24</h1>
       <Team
         name="Time 1"
-        players={["KINHO", "DIEGUINHO", "FAEL", "MAYLSON", "MAYCON"]}
+        players={["RUGGERY", "DIEGUINHO", "FABRICIO", "MAYKY", "ISRAEL"]}
       />
       <Team
         name="Time 2"
-        players={["RUGGERY", "EDU", "GUGA", "ANTHONY", "DHON"]}
+        players={["KINHO", "RAFAEL", "GUGA", "GABRIEL", "MAYLSON"]}
       />
       <Team
         name="Time 3"
-        players={["KAUAN", "PITICO", "MESSINHO", "VERTINHO", "ISRAEL"]}
+        players={["NEGUINHO", "EDU", "VAGNER", "ANTONY", "GAUCHO"]}
       />
       <Team
         name="Time 4"
-        players={["NEGUINHO", "JUVENALDO", "RAFAEL", "SAMUEL", "GALO"]}
+        players={["JUVENALDO", "FAEL", "PITICO", "VICTOR", "DHON"]}
       />
     </>
   );
