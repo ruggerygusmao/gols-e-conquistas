53757ee3f5b25604404806fb67089e1704e6d602
Ruggery Gusmão <ruggery402@gmail.com>
2024-10-11 22:59:47 -0300

    Atualizar o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import Team from "./team/Team";
 import "./style.css"
-
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
+  const [countdown, setCountdown] = useState(countdownTimer());
+
+  const countdownTimer = () => {
+    const difference = new Date('2024-11-15T00:00:00') - new Date();
+    let timerDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let timerHours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let timerMinutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let timerSeconds = Math.floor((difference % (1000 * 60)) / 1000);
+    return { timerDays, timerHours, timerMinutes, timerSeconds };
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(countdownTimer());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <>
+    <>{/*
       <h1 className="title">PELADA DOS AMIGOS - 13/04/24</h1>
       <Team
         name="Time 1"
@@ -23,6 +40,27 @@ function App() {
         name="Time 4"
         players={["NEGUINHO", "MAYLSON", "PITICO", "BIRA", "GUGA"]}
       />
+      */}
+       <div className="app">
+      <h1>Faltam para a viagem:</h1>
+      <div>
+        <span>{countdown.timerDays}</span>
+        <span>dias</span>
+      </div>
+      <div>
+        <span>{countdown.timerHours}</span>
+        <span>horas</span>
+      </div>
+      <div>
+        <span>{countdown.timerMinutes}</span>
+        <span>minutos</span>
+      </div>
+      <div>
+        <span>{countdown.timerSeconds}</span>
+        <span>segundos</span>
+      </div>
+    </div>
+      
     </>
   );
 }
