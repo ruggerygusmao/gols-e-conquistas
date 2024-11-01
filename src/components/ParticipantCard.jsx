4bdbfe72bdc522ae838ca2eea6e6b75bd1f54f8a
ruggerygusmao <ruggery402@gmail.com>
2024-11-01 15:40:09 -0300

    ajuste de fotos

--- src/components/ParticipantCard.jsx
+++ src/components/ParticipantCard.jsx
@@ -19,17 +19,17 @@ const Participants = () => {
     {
       name: "Rayane",
       imageSrc:
-        "https://media-for1-1.cdn.whatsapp.net/v/t61.24694-24/323819736_522114254107349_6344785853539912047_n.jpg?ccb=11-4&oh=01_Q5AaIFHfUitXj-KKsBYGrtJ0VTfjDew3Fl4Qksk8_WjPaWu7&oe=671D1815&_nc_sid=5e03e0&_nc_cat=102",
+        "https://media-for2-2.cdn.whatsapp.net/v/t61.24694-24/455158393_1192242188539852_8846478617552128522_n.jpg?ccb=11-4&oh=01_Q5AaIDif5OgIQvxlmFrHwfvv58ZkBPZ2jlhBWxxiaWHgQKKd&oe=6731A6A7&_nc_sid=5e03e0&_nc_cat=107",
     },
     {
       name: "Vinicius",
       imageSrc:
-        "https://media-for1-1.cdn.whatsapp.net/v/t61.24694-24/462790092_3767622536821991_1741995600321484957_n.jpg?ccb=11-4&oh=01_Q5AaIBG0vWfeldOfmo7INaUkRuQZDGmqUKuBxCOZKH-ZuHyz&oe=671CE7B1&_nc_sid=5e03e0&_nc_cat=102",
+        "https://media-for2-2.cdn.whatsapp.net/v/t61.24694-24/462790092_3767622536821991_1741995600321484957_n.jpg?ccb=11-4&oh=01_Q5AaIEN3hQYC4EXQdcMSTQia9xIcy6jju053xn-OMVsEBTMf&oe=673237F1&_nc_sid=5e03e0&_nc_cat=102",
     },
     {
       name: "Isabely",
       imageSrc:
-        "https://media-for1-1.cdn.whatsapp.net/v/t61.24694-24/462790092_3767622536821991_1741995600321484957_n.jpg?ccb=11-4&oh=01_Q5AaIBG0vWfeldOfmo7INaUkRuQZDGmqUKuBxCOZKH-ZuHyz&oe=671CE7B1&_nc_sid=5e03e0&_nc_cat=102",
+        "https://media-for2-2.cdn.whatsapp.net/v/t61.24694-24/462790092_3767622536821991_1741995600321484957_n.jpg?ccb=11-4&oh=01_Q5AaIEN3hQYC4EXQdcMSTQia9xIcy6jju053xn-OMVsEBTMf&oe=673237F1&_nc_sid=5e03e0&_nc_cat=102",
     },
   ];
 
