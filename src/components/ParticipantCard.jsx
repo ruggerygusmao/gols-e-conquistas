e6a57d608b9e51fee62c797651e8cd62611a5712
Ruggery Gusmão <ruggery402@gmail.com>
2025-02-12 00:19:40 -0300

    Atualizar o ParticipantCard.jsx

--- src/components/ParticipantCard.jsx
+++ src/components/ParticipantCard.jsx
@@ -22,12 +22,12 @@ const Participants = () => {
         "https://media-for2-2.cdn.whatsapp.net/v/t61.24694-24/455158393_1192242188539852_8846478617552128522_n.jpg?ccb=11-4&oh=01_Q5AaIDif5OgIQvxlmFrHwfvv58ZkBPZ2jlhBWxxiaWHgQKKd&oe=6731A6A7&_nc_sid=5e03e0&_nc_cat=107",
     },
     {
-      name: "Vinicius",
+      name: "Chrislayne",
       imageSrc:
         "https://media-for2-2.cdn.whatsapp.net/v/t61.24694-24/462790092_3767622536821991_1741995600321484957_n.jpg?ccb=11-4&oh=01_Q5AaIEN3hQYC4EXQdcMSTQia9xIcy6jju053xn-OMVsEBTMf&oe=673237F1&_nc_sid=5e03e0&_nc_cat=102",
     },
     {
-      name: "Isabely",
+      name: "Marcela",
       imageSrc:
         "https://media-for2-2.cdn.whatsapp.net/v/t61.24694-24/462790092_3767622536821991_1741995600321484957_n.jpg?ccb=11-4&oh=01_Q5AaIEN3hQYC4EXQdcMSTQia9xIcy6jju053xn-OMVsEBTMf&oe=673237F1&_nc_sid=5e03e0&_nc_cat=102",
     },
